4ac0f26bbfd4e8673b4994c2d61c936f394eb4fa
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-06-18 00:57:25 +0300

    added test data

---
+++ wiki_project/data/data.py
@@ -0,0 +1,9 @@
+
+test_text = 'Appium'
+text_python = 'Python'
+
+text_screen_one = 'The Free Encyclopedia\n…in over 300 languages'
+text_screen_two = 'New ways to explore'
+text_screen_three = 'Reading lists with sync'
+text_screen_four = 'Data & Privacy'
+

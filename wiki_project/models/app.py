3a981937f44140710e22da78a45120d709e7c87c
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-07-09 00:21:20 +0300

    fix

--- wiki_project/models/app.py
+++ wiki_project/models/app.py
@@ -1,12 +1,12 @@
-from wiki_project.models.pages.saved_page import SavedPage
+from wiki_project.models.pages.reading_list_page import SavedPage
 from wiki_project.models.pages.search_page import SearchPage
-from wiki_project.models.pages.start_screen_page import StartScreenPage
+from wiki_project.models.pages.main_page import MainPage
 
 
 class ApplicationManager:
     def __init__(self):
         self.search = SearchPage()
-        self.start_screen = StartScreenPage()
+        self.main_page = MainPage()
         self.saved = SavedPage()
 
 app = ApplicationManager()
